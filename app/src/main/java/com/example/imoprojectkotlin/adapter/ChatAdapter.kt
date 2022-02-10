a56af8a71df31556bf013dcc8812d6187d600ba9
User <murodjon19>
2022-02-10 15:12:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/imoprojectkotlin/adapter/ChatAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.imoprojectkotlin.adapter
+
+import android.content.Context
+
+class ChatAdapter (var context: Context, var items ArrayList<chat>) : RecycleView.Adapter<RecyclerViews.ViewHolder {
+}
---
+++ app/src/main/java/com/example/imoprojectkotlin/modul/Chat.kt
@@ -0,0 +1,5 @@
+package com.example.imoprojectkotlin.modul
+
+data class Chat (var grafile: Int, var fullname: String, var count: Int) {
+
+}
